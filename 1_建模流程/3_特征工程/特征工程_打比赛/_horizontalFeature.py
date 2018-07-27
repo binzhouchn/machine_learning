45c68dfbc0dffe2d6ff71e60fc89a470dfc64e8c
Bin Zhou <binzhouchn@gmail.com>
2018-07-27 10:21:07 +0800

    update 20180727

--- 1_建模流程/3_特征工程/特征工程_打比赛/_horizontalFeature.py
+++ 1_建模流程/3_特征工程/特征工程_打比赛/_horizontalFeature.py
@@ -7,23 +7,48 @@ __mtime__ = '2018/7/26'
 import numpy as np
 import pandas as pd
 from tqdm import tqdm, tqdm_notebook
+from sklearn.preprocessing import LabelEncoder
 
-# 对数值型特征，先进行计数和排序（横向衍生两列特征）
-
-group = 'ID'
-feature = 'feature_1'
-ftr_ = df[[group,feature]].copy()
-
-
-
-
-
-
-# 时间特征（未聚合）
-class TimeFeature(object):
+# 4. 时间特征（未聚合）
+class HorizongtalFeature(object):
     def __init__(self):
         pass
 
+    # 1. 对数值型特征，先进行计数和排序（横向衍生两列特征）
+    @staticmethod
+    def get_feats_vcrank(df, feat='', return_labelencoder=False):
+        # 0.1 计数特征 value_counts
+        ftr_ = df[feat].value_counts()
+        ftr_ = pd.DataFrame(list(zip(ftr_.index,ftr_.values)),columns=[feat,feat+'_'+'vcounts'])
+        df = df.merge(ftr_, 'left', on=feat)
+        # 0.2 排序特征
+        le = LabelEncoder()
+        ftr_ = le.fit_transform(df[feat])
+        df[feat+'_'+'rank'] = ftr_
+        if return_labelencoder:
+            return le, df
+        return df
+
+    # 2. 针对同类特征群（比如消费，浏览记录等）横向扩展，计算一些统计量作为特征
+    @staticmethod
+    def get_feats_agg_desc(df, feat_cols=None):
+        df = df.copy()
+        _df = df[feat_cols]
+
+        funcs = ['count', 'min', 'mean', 'median', 'max', 'sum', 'std', 'var', 'sem', 'skew']
+        for f in funcs:
+            df['row_' + f] = _df.__getattr__(f)(1)
+        if len(feat_cols) > 3:
+            df['row_kurt'] = _df.kurt(1)
+        df['row_q1'] = _df.quantile(0.25, 1)
+        df['row_q3'] = _df.quantile(0.75, 1)
+        df['row_q3_q1'] = df['row_q3'] - df['row_q1']
+        df['row_max_min'] = df['row_max'] - df['row_min']
+        df['row_cv'] = df['row_std'] / (df['row_mean'] + 10 ** -8)  # 变异系数
+        df['row_cv_reciprocal'] = df['row_mean'] / (df['row_std'] + 10 ** -8)
+        return df
+
+    # 4. 时间特征（未聚合）
     @staticmethod
     def get_feats_time(data, group=None, feats=None, ts='ts'):
         """
