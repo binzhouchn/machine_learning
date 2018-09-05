b866f8d035d4ae46e08d9e4c70cc25b224a8c769
Bin Zhou <binzhouchn@gmail.com>
2018-09-05 10:57:53 +0800

    update 20180905

--- 2_面试中常见的问题/code/scorecard.py
+++ 2_面试中常见的问题/code/scorecard.py
@@ -7,31 +7,44 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.linear_model import LogisticRegressionCV
+import statsmodels.api as sm
+from sklearn.ensemble import RandomForestClassifier
 
 def CareerYear(x):
+    #对工作年限进行转换
     if x.find('n/a') > -1:
         return -1
-    elif x.find("10+")>-1:
+    elif x.find("10+")>-1:   #将"10＋years"转换成 11
         return 11
-    elif x.find('< 1') > -1:
+    elif x.find('< 1') > -1:  #将"< 1 year"转换成 0
         return 0
     else:
-        return int(re.sub("\D", "", x))
+        return int(re.sub("\D", "", x))   #其余数据，去掉"years"并转换成整数
 
 
 def DescExisting(x):
-    x2 = str(x)
-    if x2 == 'nan':
+    #将desc变量转换成有记录和无记录两种
+    if type(x).__name__ == 'float':
         return 'no desc'
     else:
         return 'desc'
 
 
-def ConvertDateStr(x,format):
+def ConvertDateStr(x):
+    mth_dict = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10,
+                'Nov': 11, 'Dec': 12}
     if str(x) == 'nan':
         return datetime.datetime.fromtimestamp(time.mktime(time.strptime('9900-1','%Y-%m')))
+        #time.mktime 不能读取1970年之前的日期
     else:
-        return datetime.datetime.fromtimestamp(time.mktime(time.strptime(x,format)))
+        yr = int(x[4:6])
+        if yr <=17:
+            yr = 2000+yr
+        else:
+            yr = 1900 + yr
+        mth = mth_dict[x[:3]]
+        return datetime.datetime(yr,mth,1)
 
 
 def MonthGap(earlyDate, lateDate):
@@ -57,7 +70,7 @@ def MakeupMissing(x):
 # 2，选择合适的建模样本
 # 3，数据集划分成训练集和测试集
 #allData = pd.read_csv('C:/Users/OkO/Desktop/Financial Data Analsys/3nd Series/Data/application.csv',header = 0)
-allData = pd.read_csv('/Users/Code/xiaoxiang/第3期/Data/application.csv',header = 0)
+allData = pd.read_csv('C:/Users/OkO/Desktop/Financial Data Analsys/3nd Series/Data/application.csv',header = 0, encoding = 'latin1')
 allData['term'] = allData['term'].apply(lambda x: int(x.replace(' months','')))
 
 # 处理标签：Fully Paid是正常用户；Charged Off是违约用户
@@ -94,8 +107,8 @@ trainData['emp_length_clean'] = trainData['emp_length'].map(CareerYear)
 trainData['desc_clean'] = trainData['desc'].map(DescExisting)
 
 # 处理日期。earliest_cr_line的格式不统一，需要统一格式且转换成python的日期
-trainData['app_date_clean'] = trainData['issue_d'].map(lambda x: ConvertDateStr(x,'%b-%y'))
-trainData['earliest_cr_line_clean'] = trainData['earliest_cr_line'].map(lambda x: ConvertDateStr(x,'%b-%y'))
+trainData['app_date_clean'] = trainData['issue_d'].map(lambda x: ConvertDateStr(x))
+trainData['earliest_cr_line_clean'] = trainData['earliest_cr_line'].map(lambda x: ConvertDateStr(x))
 
 # 处理mths_since_last_delinq。注意原始值中有0，所以用－1代替缺失
 trainData['mths_since_last_delinq_clean'] = trainData['mths_since_last_delinq'].map(lambda x:MakeupMissing(x))
@@ -141,36 +154,37 @@ for var in cat_features:
     valueCounts = len(set(trainData[var]))
     print valueCounts
     if valueCounts > 5:
-        more_value_features.append(var)
+        more_value_features.append(var)  #取值超过5的变量，需要bad rate编码，再用卡方分箱法进行分箱
     else:
         less_value_features.append(var)
 
 # （i）当取值<5时：如果每种类别同时包含好坏样本，无需分箱；如果有类别只包含好坏样本的一种，需要合并
-merge_bin_dict = {}
-var_bin_list = []
+merge_bin_dict = {}  #存放需要合并的变量，以及合并方法
+var_bin_list = []   #由于某个取值没有好或者坏样本而需要合并的变量
 for col in less_value_features:
     binBadRate = BinBadRate(trainData, col, 'y')[0]
-    if min(binBadRate.values()) == 0 :
+    if min(binBadRate.values()) == 0 :  #由于某个取值没有坏样本而进行合并
         print '{} need to be combined due to 0 bad rate'.format(col)
         combine_bin = MergeBad0(trainData, col, 'y')
         merge_bin_dict[col] = combine_bin
         newVar = col + '_Bin'
-        trainData[newVar] = trainData[col].map(merged_bin)
+        trainData[newVar] = trainData[col].map(combine_bin)
         var_bin_list.append(newVar)
-    if max(binBadRate.values()) == 1:
+    if max(binBadRate.values()) == 1:    #由于某个取值没有好样本而进行合并
         print '{} need to be combined due to 0 good rate'.format(col)
         combine_bin = MergeBad0(trainData, col, 'y',direction = 'good')
         merge_bin_dict[col] = combine_bin
         newVar = col + '_Bin'
-        trainData[newVar] = trainData[col].map(merged_bin)
+        trainData[newVar] = trainData[col].map(combine_bin)
         var_bin_list.append(newVar)
 
+#less_value_features里剩下不需要合并的变量
 less_value_features = [i for i in less_value_features if i + '_Bin' not in var_bin_list]
 
 # （ii）当取值>5时：用bad rate进行编码，放入连续型变量里
-br_encoding_dict = {}
+br_encoding_dict = {}   #记录按照bad rate进行编码的变量，及编码方式
 for col in more_value_features:
-    br_encoding = BadRateEncoding(df, col, target)
+    br_encoding = BadRateEncoding(trainData, col, 'y')
     trainData[col+'_br_encoding'] = br_encoding['encoding']
     br_encoding_dict[col] = br_encoding['bad_rate']
     num_features.append(col+'_br_encoding')
@@ -179,14 +193,15 @@ for col in more_value_features:
 continous_merged_dict = {}
 for col in num_features:
     print "{} is in processing".format(col)
-    if -1 not in set(trainData[col]):
-        max_interval = 5
+    if -1 not in set(trainData[col]):   #－1会当成特殊值处理。如果没有－1，则所有取值都参与分箱
+        max_interval = 5   #分箱后的最多的箱数
         cutOff = ChiMerge(trainData, col, 'y', max_interval=max_interval,special_attribute=[],minBinPcnt=0)
         trainData[col+'_Bin'] = trainData[col].map(lambda x: AssignBin(x, cutOff,special_attribute=[]))
-        monotone = BadRateMonotone(trainData, col+'_Bin', 'y')
+        monotone = BadRateMonotone(trainData, col+'_Bin', 'y')   # 检验分箱后的单调性是否满足
         while(not monotone):
+            # 检验分箱后的单调性是否满足。如果不满足，则缩减分箱的个数。
             max_interval -= 1
-            cutOff = ChiMerge(trainData, col, target, max_interval=max_interval, special_attribute=[],
+            cutOff = ChiMerge(trainData, col, 'y', max_interval=max_interval, special_attribute=[],
                                           minBinPcnt=0)
             trainData[col + '_Bin'] = trainData[col].map(lambda x: AssignBin(x, cutOff, special_attribute=[]))
             if max_interval == 2:
@@ -198,19 +213,21 @@ for col in num_features:
         var_bin_list.append(newVar)
     else:
         max_interval = 5
-        cutOff = ChiMerge(trainData, col, target, max_interval=max_interval, special_attribute=[-1],
+        # 如果有－1，则除去－1后，其他取值参与分箱
+        cutOff = ChiMerge(trainData, col, 'y', max_interval=max_interval, special_attribute=[-1],
                                       minBinPcnt=0)
         trainData[col + '_Bin'] = trainData[col].map(lambda x: AssignBin(x, cutOff, special_attribute=[-1]))
-        monotone = BadRateMonotone(trainData, col + '_Bin', 'y')
+        monotone = BadRateMonotone(trainData, col + '_Bin', 'y',['Bin -1'])
         while (not monotone):
             max_interval -= 1
-            cutOff = ChiMerge(trainData, col, target, max_interval=max_interval, special_attribute=[-1],
+            # 如果有－1，－1的bad rate不参与单调性检验
+            cutOff = ChiMerge(trainData, col, 'y', max_interval=max_interval, special_attribute=[-1],
                                           minBinPcnt=0)
             trainData[col + '_Bin'] = trainData[col].map(lambda x: AssignBin(x, cutOff, special_attribute=[-1]))
-            if max_interval == 2:
-                # 当分箱数为2时，必然单调
+            if max_interval == 3:
+                # 当分箱数为3-1=2时，必然单调
                 break
-            monotone = BadRateMonotone(trainData, col + '_Bin', 'y')
+            monotone = BadRateMonotone(trainData, col + '_Bin', 'y',['Bin -1'])
         newVar = col + '_Bin'
         trainData[newVar] = trainData[col].map(lambda x: AssignBin(x, cutOff, special_attribute=[-1]))
         var_bin_list.append(newVar)
@@ -222,12 +239,18 @@ for col in num_features:
 '''
 WOE_dict = {}
 IV_dict = {}
+# 分箱后的变量进行编码，包括：
+# 1，初始取值个数小于5，且不需要合并的类别型变量。存放在less_value_features中
+# 2，初始取值个数小于5，需要合并的类别型变量。合并后新的变量存放在var_bin_list中
+# 3，初始取值个数超过5，需要合并的类别型变量。合并后新的变量存放在var_bin_list中
+# 4，连续变量。分箱后新的变量存放在var_bin_list中
 all_var = var_bin_list  + less_value_features
 for var in all_var:
     woe_iv = CalcWOE(trainData, var, 'y')
     WOE_dict[var] = woe_iv['WOE']
     IV_dict[var] = woe_iv['IV']
 
+#将变量IV值进行降序排列，方便后续挑选变量
 IV_dict_sorted = sorted(IV_dict.items(), key=lambda x: x[1], reverse=True)
 
 IV_values = [i[1] for i in IV_dict_sorted]
@@ -239,7 +262,7 @@ plt.bar(range(len(IV_values)),IV_values)
 
 
 '''
-第五步：单变量分析和多变量分析：
+第五步：单变量分析和多变量分析，均基于WOE编码后的值。
 （1）选择IV高于0.01的变量
 （2）比较两两线性相关性。如果相关系数的绝对值高于阈值，剔除IV较低的一个
 '''
@@ -255,14 +278,17 @@ for var in short_list:
     trainData[newVar] = trainData[var].map(WOE_dict[var])
     short_list_2.append(newVar)
 
-
+#对于上一步的结果，计算相关系数矩阵，并画出热力图进行数据可视化
 trainDataWOE = trainData[short_list_2]
 f, ax = plt.subplots(figsize=(10, 8))
 corr = trainDataWOE.corr()
 sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True),square=True, ax=ax)
 
 
-
+#两两间的线性相关性检验
+#1，将候选变量按照IV进行降序排列
+#2，计算第i和第i+1的变量的线性相关系数
+#3，对于系数超过阈值的两个变量，剔除IV较低的一个
 deleted_index = []
 cnt_vars = len(high_IV_sorted)
 for i in range(cnt_vars):
@@ -283,24 +309,131 @@ for i in range(cnt_vars):
                 deleted_index.append(i)
 
 multi_analysis_vars_1 = [high_IV_sorted[i][0]+"_WOE" for i in range(cnt_vars) if i not in deleted_index]
-'''
-int_rate_clean_Bin_WOE
-zip_code_br_encoding_Bin_WOE
-purpose_br_encoding_Bin_WOE
-annual_inc_Bin_WOE
-addr_state_br_encoding_Bin_WOE
-inq_last_6mths_Bin_WOE
-mths_since_last_record_clean_Bin_WOE
-open_acc_Bin_WOE
-dti_Bin_WOE
-emp_length_clean_Bin_WOE
-total_acc_Bin_WOE
-'''
+
 
 '''
 多变量分析：VIF
 '''
 X = np.matrix(trainData[multi_analysis_vars_1])
 VIF_list = [variance_inflation_factor(X, i) for i in range(X.shape[1])]
-# 最大的VIF是1.2556351764181208，因此这一步认为没有多重共线性
+max_VIF = max(VIF_list)
+print max_VIF
+# 最大的VIF是1.32267733123，因此这一步认为没有多重共线性
 multi_analysis = multi_analysis_vars_1
+
+
+
+'''
+第六步：逻辑回归模型。
+要求：
+1，变量显著
+2，符号为负
+'''
+### (1)将多变量分析的后变量带入LR模型中
+y = trainData['y']
+X = trainData[multi_analysis]
+X['intercept'] = [1]*X.shape[0]
+
+
+LR = sm.Logit(y, X).fit()
+summary = LR.summary()
+pvals = LR.pvalues
+pvals = pvals.to_dict()
+
+# ### 有些变量不显著，需要逐步剔除
+varLargeP = {k: v for k,v in pvals.items() if v >= 0.1}
+varLargeP = sorted(varLargeP.iteritems(), key=lambda d:d[1], reverse = True)
+while(len(varLargeP) > 0 and len(multi_analysis) > 0):
+    # 每次迭代中，剔除最不显著的变量，直到
+    # (1) 剩余所有变量均显著
+    # (2) 没有特征可选
+    varMaxP = varLargeP[0][0]
+    print varMaxP
+    if varMaxP == 'intercept':
+        print 'the intercept is not significant!'
+        break
+    multi_analysis.remove(varMaxP)
+    y = trainData['y']
+    X = trainData[multi_analysis]
+    X['intercept'] = [1] * X.shape[0]
+
+    LR = sm.Logit(y, X).fit()
+    #summary = LR.summary()
+    pvals = LR.pvalues
+    pvals = pvals.to_dict()
+    varLargeP = {k: v for k, v in pvals.items() if v >= 0.1}
+    varLargeP = sorted(varLargeP.iteritems(), key=lambda d: d[1], reverse=True)
+
+'''
+int_rate_clean_Bin_WOE                 -0.923325
+zip_code_br_encoding_Bin_WOE           -0.913290
+purpose_br_encoding_Bin_WOE            -0.842351
+annual_inc_Bin_WOE                     -0.778415
+inq_last_6mths_Bin_WOE                 -0.871196
+addr_state_br_encoding_Bin_WOE         -0.618244
+mths_since_last_record_clean_Bin_WOE   -0.740668
+emp_length_clean_Bin_WOE               -0.702146
+home_ownership_Bin_WOE                 -0.396530
+dti_Bin_WOE                            -0.709372
+intercept                              -2.095788
+'''
+#
+#
+saveModel =open(folderOfData+'LR_Model_Normal.pkl','w')
+pickle.dump(LR,saveModel)
+saveModel.close()
+#
+#
+
+
+#尝试用L1约束#
+## use cross validation to select the best regularization parameter
+multi_analysis = multi_analysis_vars_1
+X = trainData[multi_analysis]   #by default  LogisticRegressionCV() fill fit the intercept
+X = np.matrix(X)
+y = trainData['y']
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+X_train.shape, y_train.shape
+#
+model_parameter = {}
+for C_penalty in np.arange(0.005, 0.2,0.005):
+    for bad_weight in range(2, 101, 2):
+        print C_penalty, bad_weight
+        LR_model_2 = LogisticRegressionCV(Cs=[C_penalty], penalty='l1', solver='liblinear', class_weight={1:bad_weight, 0:1})
+        LR_model_2_fit = LR_model_2.fit(X_train,y_train)
+        y_pred = LR_model_2_fit.predict_proba(X_test)[:,1]
+        scorecard_result = pd.DataFrame({'prob':y_pred, 'target':y_test})
+        #performance = KS_AR(scorecard_result,'prob','target')
+        KS = performance['KS']
+        model_parameter[(C_penalty, bad_weight)] = KS
+
+# endtime = datetime.datetime.now()
+# print (endtime - starttime).seconds
+
+
+
+# 用随机森林法估计变量重要性#
+#
+var_WOE_list = multi_analysis_vars_1
+X = trainData[var_WOE_list]
+X = np.matrix(X)
+y = trainData['y']
+y = np.array(y)
+
+RFC = RandomForestClassifier()
+RFC_Model = RFC.fit(X,y)
+features_rfc = trainData[var_WOE_list].columns
+featureImportance = {features_rfc[i]:RFC_Model.feature_importances_[i] for i in range(len(features_rfc))}
+featureImportanceSorted = sorted(featureImportance.iteritems(),key=lambda x: x[1], reverse=True)
+# we selecte the top 10 features
+features_selection = [k[0] for k in featureImportanceSorted[:8]]
+
+y = trainData['y']
+X = trainData[features_selection]
+X['intercept'] = [1]*X.shape[0]
+
+
+LR = sm.Logit(y, X).fit()
+summary = LR.summary()
--- 2_面试中常见的问题/code/scorecard_functions.py
+++ 2_面试中常见的问题/code/scorecard_functions.py
@@ -93,7 +93,7 @@ def ChiMerge(df, col, target, max_interval=5,special_attribute=[],minBinPcnt=0):
             split_x = SplitData(df2, col, 100)
             df2['temp'] = df2[col].map(lambda x: AssignGroup(x, split_x))
         else:
-            df2['temp'] = df[col]
+            df2['temp'] = df2[col]
         # 总体bad rate将被用来计算expected bad count
         (binBadRate, regroup, overallRate) = BinBadRate(df2, 'temp', target, grantRateIndicator=1)
 
@@ -119,7 +119,7 @@ def ChiMerge(df, col, target, max_interval=5,special_attribute=[],minBinPcnt=0):
             best_comnbined = chisqList.index(min(chisqList))
             groupIntervals[best_comnbined] = groupIntervals[best_comnbined] + groupIntervals[best_comnbined+1]
             # after combining two intervals, we need to remove one of them
-            groupIntervals.remove(groupIntervals[best_comnbined])
+            groupIntervals.remove(groupIntervals[best_comnbined+1])
         groupIntervals = [sorted(i) for i in groupIntervals]
         cutOffPoints = [max(i) for i in groupIntervals[:-1]]
 
@@ -168,7 +168,7 @@ def ChiMerge(df, col, target, max_interval=5,special_attribute=[],minBinPcnt=0):
             valueCounts['pcnt'] = valueCounts['temp'].apply(lambda x: x * 1.0 / N)
             valueCounts = valueCounts.sort_index()
             minPcnt = min(valueCounts['pcnt'])
-            while minPcnt < 0.05 and len(cutOffPoints) > 2:
+            while minPcnt < minBinPcnt and len(cutOffPoints) > 2:
                 # 找出占比最小的箱
                 indexForMinPcnt = valueCounts[valueCounts['pcnt'] == minPcnt].index.tolist()[0]
                 # 如果占比最小的箱是最后一箱，则需要和上一个箱进行合并，也就意味着分裂点cutOffPoints中的最后一个需要移除
@@ -224,6 +224,11 @@ def UnsupervisedSplitBin(df,var,numOfSplit = 5, method = 'equal freq'):
 
 
 def AssignGroup(x, bin):
+    '''
+    :param x: 某个变量的某个取值
+    :param bin: 上述变量的分箱结果
+    :return: x在分箱结果下的映射
+    '''
     N = len(bin)
     if x<=min(bin):
         return min(bin)
@@ -252,10 +257,10 @@ def BadRateEncoding(df, col, target):
 
 def AssignBin(x, cutOffPoints,special_attribute=[]):
     '''
-    :param x: the value of variable
-    :param cutOffPoints: the ChiMerge result for continous variable
-    :param special_attribute:  the special attribute which should be assigned separately
-    :return: bin number, indexing from 0
+    :param x: 某个变量的某个取值
+    :param cutOffPoints: 上述变量的分箱结果，用切分点表示
+    :param special_attribute:  不参与分箱的特殊取值
+    :return: 分箱后的对应的第几个箱，从0开始
     for example, if cutOffPoints = [10,20,30], if x = 7, return Bin 0. If x = 35, return Bin 3
     '''
     numBin = len(cutOffPoints) + 1 + len(special_attribute)
@@ -272,35 +277,13 @@ def AssignBin(x, cutOffPoints,special_attribute=[]):
                 return 'Bin {}'.format(i+1)
 
 
-def MaximumBinPcnt(df,col):
-    N = df.shape[0]
-    total = df.groupby([col])[col].count()
-    pcnt = total*1.0/N
-    return max(pcnt)
-
-def BinPcnt(df,col):
-    N = df.shape[0]
-    total = df.groupby([col])[col].count()
-    pcnt = total*1.0/N
-    return {'min':min(pcnt),'max':max(pcnt), 'each pcnt': pcnt.to_dict()}
-
-def MergeByCondition(x,condition_list):
-    #condition_list是条件列表。满足第几个condition，就输出几
-    s = 0
-    for condition in condition_list:
-        if eval(str(x)+condition):
-            return s
-        else:
-            s+=1
-
-
 
 def CalcWOE(df, col, target):
     '''
-    :param df: dataframe containing feature and target
-    :param col: the feature that needs to be calculated the WOE and iv, usually categorical type
-    :param target: good/bad indicator
-    :return: WOE and IV in a dictionary
+    :param df: 包含需要计算WOE的变量和目标变量
+    :param col: 需要计算WOE、IV的变量，必须是分箱后的变量，或者不需要分箱的类别型变量
+    :param target: 目标变量，0、1表示好、坏
+    :return: 返回WOE和IV
     '''
     total = df.groupby([col])[target].count()
     total = pd.DataFrame({'total': total})
@@ -322,14 +305,14 @@ def CalcWOE(df, col, target):
     IV = sum(IV)
     return {"WOE": WOE_dict, 'IV':IV}
 
-## determine whether the bad rate is monotone along the sortByVar
+## 判断某变量的坏样本率是否单调
 def BadRateMonotone(df, sortByVar, target,special_attribute = []):
     '''
-    :param df: the dataset contains the column which should be monotone with the bad rate and bad column
-    :param sortByVar: the column which should be monotone with the bad rate
-    :param target: the bad column
-    :param special_attribute: some attributes should be excluded when checking monotone
-    :return:
+    :param df: 包含检验坏样本率的变量，和目标变量
+    :param sortByVar: 需要检验坏样本率的变量
+    :param target: 目标变量，0、1表示好、坏
+    :param special_attribute: 不参与检验的特殊值
+    :return: 坏样本率单调与否
     '''
     df2 = df.loc[~df[sortByVar].isin(special_attribute)]
     if len(set(df2[sortByVar])) <= 2:
@@ -337,29 +320,30 @@ def BadRateMonotone(df, sortByVar, target,special_attribute = []):
     regroup = BinBadRate(df2, sortByVar, target)[1]
     combined = zip(regroup['total'],regroup['bad'])
     badRate = [x[1]*1.0/x[0] for x in combined]
-    badRateMonotone = [badRate[i]<badRate[i+1] and badRate[i] < badRate[i-1] or badRate[i]>badRate[i+1] and badRate[i] > badRate[i-1]
+    badRateNotMonotone = [badRate[i]<badRate[i+1] and badRate[i] < badRate[i-1] or badRate[i]>badRate[i+1] and badRate[i] > badRate[i-1]
                        for i in range(1,len(badRate)-1)]
-    Monotone = len(set(badRateMonotone))
-    if Monotone == 1:
-        return True
-    else:
+    if True in badRateNotMonotone:
         return False
+    else:
+        return True
 
 
 
 
-### If we find any categories with 0 bad, then we combine these categories with that having smallest non-zero bad rate
+
 def MergeBad0(df,col,target, direction='bad'):
     '''
-     :param df: dataframe containing feature and target
-     :param col: the feature that needs to be calculated the WOE and iv, usually categorical type
-     :param target: good/bad indicator
-     :return: WOE and IV in a dictionary
+     :param df: 包含检验0％或者100%坏样本率
+     :param col: 分箱后的变量或者类别型变量。检验其中是否有一组或者多组没有坏样本或者没有好样本。如果是，则需要进行合并
+     :param target: 目标变量，0、1表示好、坏
+     :return: 合并方案，使得每个组里同时包含好坏样本
      '''
     regroup = BinBadRate(df, col, target)[1]
     if direction == 'bad':
+        # 如果是合并0坏样本率的组，则跟最小的非0坏样本率的组进行合并
         regroup = regroup.sort_values(by  = 'bad_rate')
     else:
+        # 如果是合并0好样本样本率的组，则跟最小的非0好样本率的组进行合并
         regroup = regroup.sort_values(by='bad_rate',ascending=False)
     regroup.index = range(regroup.shape[0])
     col_regroup = [[i] for i in regroup[col]]
@@ -379,48 +363,3 @@ def MergeBad0(df,col,target, direction='bad'):
         for g2 in col_regroup2[i]:
             newGroup[g2] = 'Bin '+str(i)
     return newGroup
-
-
-
-### Calculate the KS and AR for the socrecard model
-def KS_AR(df, score, target):
-    '''
-    :param df: the dataset containing probability and bad indicator
-    :param score:
-    :param target:
-    :return:
-    '''
-    total = df.groupby([score])[target].count()
-    bad = df.groupby([score])[target].sum()
-    all = pd.DataFrame({'total':total, 'bad':bad})
-    all['good'] = all['total'] - all['bad']
-    all[score] = all.index
-    all = all.sort_values(by=score,ascending=False)
-    all.index = range(len(all))
-    all['badCumRate'] = all['bad'].cumsum() / all['bad'].sum()
-    all['goodCumRate'] = all['good'].cumsum() / all['good'].sum()
-    all['totalPcnt'] = all['total'] / all['total'].sum()
-    arList = [0.5 * all.loc[0, 'badCumRate'] * all.loc[0, 'totalPcnt']]
-    for j in range(1, len(all)):
-        ar0 = 0.5 * sum(all.loc[j - 1:j, 'badCumRate']) * all.loc[j, 'totalPcnt']
-        arList.append(ar0)
-    arIndex = (2 * sum(arList) - 1) / (all['good'].sum() * 1.0 / all['total'].sum())
-    KS = all.apply(lambda x: x.badCumRate - x.goodCumRate, axis=1)
-    return {'AR':arIndex, 'KS': max(KS)}
-
-def Predict_LR(x, var_list, coef_dict, prob=False):
-    '''
-    :param x: WOE编码后的变量
-    :param var_list: 入模变量列表
-    :param coef_dict: 逻辑回归系数列表
-    :param prob: 如果返回概率，则设定为True。默认是返回log odds
-    :return: 返回概率或者log odds
-    '''
-    s = coef_dict['intercept']
-    for var in var_list:
-        s += x[var]*coef_dict[var]
-    if prob == True:
-        y = 1.0/(1+np.exp(-s))
-        return y
-    else:
-        return s
