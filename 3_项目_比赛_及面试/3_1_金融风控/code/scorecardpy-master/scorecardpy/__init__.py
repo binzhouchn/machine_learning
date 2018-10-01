8256e5433bfc8f313094985afe4b88e740e92a36
Bin Zhou <binzhouchn@gmail.com>
2018-10-01 20:48:42 +0800

    update 20181001

---
+++ 3_项目_比赛_及面试/3_1_金融风控/code/scorecardpy-master/scorecardpy/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+
+from scorecardpy.germancredit import germancredit
+from scorecardpy.split_df import split_df
+from scorecardpy.info_value import iv
+# from .info_ent_indx_gini import (ig, ie)
+from scorecardpy.var_filter import var_filter
+from scorecardpy.woebin import (woebin, woebin_ply, woebin_plot, woebin_adj)
+from scorecardpy.perf import (perf_eva, perf_psi)
+from scorecardpy.scorecard import (scorecard, scorecard_ply)
+
