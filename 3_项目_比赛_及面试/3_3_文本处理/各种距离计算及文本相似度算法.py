7901770467b0f5e39c31c62297b63a43942c66d3
Bin Zhou <binzhouchn@gmail.com>
2018-11-14 21:57:52 +0800

    update 20181114

--- 3_项目_比赛_及面试/3_3_文本处理/各种距离计算及文本相似度算法.py
+++ 3_项目_比赛_及面试/3_3_文本处理/各种距离计算及文本相似度算法.py
@@ -3,29 +3,27 @@ import math
 
 # 依赖包numpy、python-Levenshtein、scipy
 
-
+# euclidean,欧式距离算法，传入参数为两个向量，返回值为欧式距离
 def Euclidean(vec1, vec2):
     npvec1, npvec2 = np.array(vec1), np.array(vec2)
     return math.sqrt(((npvec1-npvec2)**2).sum())
-# euclidean,欧式距离算法，传入参数为两个向量，返回值为欧式距离
 
 
+# Manhattan_Distance,曼哈顿距离
 def Manhattan(vec1, vec2):
     npvec1, npvec2 = np.array(vec1), np.array(vec2)
     return np.abs(npvec1-npvec2).sum()
-# Manhattan_Distance,曼哈顿距离
 
 
+# Chebyshev_Distance,切比雪夫距离
 def Chebyshev(vec1, vec2):
     npvec1, npvec2 = np.array(vec1), np.array(vec2)
     return max(np.abs(npvec1-npvec2))
-# Chebyshev_Distance,切比雪夫距离
 
 
+# MinkowskiDistance 闵可夫斯基距离，其实就是上面三种距离的集合，这里就不重复了。
 def Minkowski(vec1, vec2, params):
     pass
-# MinkowskiDistance 闵可夫斯基距离，其实就是上面三种距离的集合，这里就不重复了。
-
 
 # def Standardized_Euclidean(vec1, vec2, v):
 #     from scipy import spatial
@@ -49,15 +47,9 @@ def mahalanobis(*args, v1=None, v2=None): # v1,v2包含在args中
 print(mahalanobis([x1,x2,x3,x4],v1=x1,v2=x2))
 
 
-
-def Edit_distance_str(str1, str2):
-    import Levenshtein
-    edit_distance_distance = Levenshtein.distance(str1, str2)
-    similarity = 1-(edit_distance_distance/max(len(str1), len(str2)))
-    return {'Distance': edit_distance_distance, 'Similarity': similarity}
-# Levenshtein distance,编辑距离，用于计算两个字符串之间的编辑距离，传入参数为两个字符串
-
-
+# 针对列表改写的编辑距离，在NLP领域中，计算两个文本的相似度，是基于句子中词和词之间的差异。
+# 如果使用传统的编辑距离算法，则计算的为文本中字与字之间的编辑次数。这里根据编辑距离的思维，
+# 将编辑距离中的处理字符串中的字符对象，变成处理list中每个元素
 def Edit_distance_array(str_ary1, str_ary2):
     len_str_ary1 = len(str_ary1) + 1
     len_str_ary2 = len(str_ary2) + 1
@@ -77,14 +69,11 @@ def Edit_distance_array(str_ary1, str_ary2):
     distance = int(matrix[-1])
     similarity = 1-int(matrix[-1])/max(len(str_ary1), len(str_ary2))
     return {'Distance': distance, 'Similarity': similarity}
-# 针对列表改写的编辑距离，在NLP领域中，计算两个文本的相似度，是基于句子中词和词之间的差异。
-# 如果使用传统的编辑距离算法，则计算的为文本中字与字之间的编辑次数。这里根据编辑距离的思维，
-# 将编辑距离中的处理字符串中的字符对象，变成处理list中每个元素
 
-
-def Cosine(vec1, vec2):
-    npvec1, npvec2 = np.array(vec1), np.array(vec2)
-    return npvec1.dot(npvec2)/(math.sqrt((npvec1**2).sum()) * math.sqrt((npvec2**2).sum()))
 # Cosine，余弦夹角
 # 机器学习中借用这一概念来衡量样本向量之间的差异。
 # 也可以使用在余弦相似度算法中
+def Cosine(vec1, vec2):
+    npvec1, npvec2 = np.array(vec1), np.array(vec2)
+    return npvec1.dot(npvec2)/(math.sqrt((npvec1**2).sum()) * math.sqrt((npvec2**2).sum()))
+
